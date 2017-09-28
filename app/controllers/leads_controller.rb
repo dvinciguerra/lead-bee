36b12f9bf7f1e8027684c5f92cbfa53b35dba5e2
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-09-27 23:15:43 -0300

    Refactor lead controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,57 +1,16 @@
 class LeadsController < ApplicationController
-  before_action :set_lead, only: [:show, :edit, :update, :destroy]
 
-  # GET /leads
-  def index
-    @leads = Lead.all
-  end
-
-  # GET /leads/1
-  def show
-  end
-
-  # GET /leads/new
-  def new
-    @lead = Lead.new
-  end
-
-  # GET /leads/1/edit
-  def edit
-  end
-
-  # POST /leads
   def create
     @lead = Lead.new(lead_params)
 
     if @lead.save
-      redirect_to @lead, notice: 'Lead was successfully created.'
+			redirect_to root_url(@lead), notice: t('.success')
     else
-      render :new
+      render :root
     end
-  end
-
-  # PATCH/PUT /leads/1
-  def update
-    if @lead.update(lead_params)
-      redirect_to @lead, notice: 'Lead was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /leads/1
-  def destroy
-    @lead.destroy
-    redirect_to leads_url, notice: 'Lead was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lead
-      @lead = Lead.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
     def lead_params
       params.require(:lead).permit(:name, :email, :age, :job)
     end
