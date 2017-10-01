08f27fa9003fbf91e86fcfd2909c9865a801e647
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-10-01 18:41:06 -0300

    Add the app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  # admin dashboard for lead administration (fake auth)
-	
 
-  # public register of contact lead
-  resources :leads, only: [:create]
+	# private /admin
+  scope :admin, module: 'admin' do
+		resources :leads
+		resources :segmentation_conditions 
+		resources :segmentations do 
+			get '/search', to: 'segmentations#search'
+		end
+    get :signin,  to: 'signin#index'
+    post :authenticate,  to: 'signin#authenticate'
 
+    get '/', to: 'home#index', as: 'admin_index'
+  end
+
+  # landing page and lead register
+  post :leads, to: 'home#create', as: 'create_lead'
   root to: 'home#index'
 end
